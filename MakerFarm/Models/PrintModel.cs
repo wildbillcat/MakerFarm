59bcf5be4ad28061d6b206546fbe78c7531186f8
Patrick McMorran <kingrolloinc@sbcglobal.net>
2013-10-23 22:55:40 -0400

    Initial Commit

---
+++ MakerFarm/Models/PrintModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MakerFarm.Models
+{
+    public class PrintModel
+    {
+        long ID { get; set; }
+        string FileName { get; set; }
+        string UserID { get; set; }
+        double PrintCost { get; set; }
+        DateTime SubmissionTime { get; set; }
+        DateTime PrintTime { get; set; }
+        DateTime CompletionTime { get; set; }
+    }
+}
