0080b032ed9f595344b6e7db7a6b8058ee8a6f4c
Patrick McMorran <patrick.mcmorran@yale.edu>
2013-10-24 13:19:55 -0400

    Started working on a Model for 3D Print submissions

--- MakerFarm/Models/PrintModel.cs
+++ MakerFarm/Models/PrintModel.cs
@@ -1,18 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data.Entity;
 
 namespace MakerFarm.Models
 {
-    public class PrintModel
+    public class Print
     {
         long ID { get; set; }
         string FileName { get; set; }
         string UserID { get; set; }
+        string YDCAssetID { get; set; }
+        string MaterialColor { get; set; }
+        double StaffProcessingFee { get; set; }
         double PrintCost { get; set; }
         DateTime SubmissionTime { get; set; }
-        DateTime PrintTime { get; set; }
+        DateTime ToolpathTime { get; set; }
         DateTime CompletionTime { get; set; }
+        Int16 AuthorizedAttempts { get; set; }
+        Int16 PrinterType { get; set; }
+    }
+
+    public class PrintDBContext : DbContext
+    {
+        public DbSet<Print> Prints { get; set; }
     }
 }
