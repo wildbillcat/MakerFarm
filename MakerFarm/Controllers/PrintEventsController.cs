3bc436aa8c50c5a44dfbf9af4a485344d7cb6223
Patrick McMorran <patrick.mcmorran@yale.edu>
2013-11-15 10:15:04 -0500

    Scaffolded PrinterErrorTypes and PrintEvents.

---
+++ MakerFarm/Controllers/PrintEventsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MakerFarm.Models;
+
+namespace MakerFarm.Controllers
+{
+    public class PrintEventsController : Controller
+    {
+        private MakerfarmDBContext db = new MakerfarmDBContext();
+
+        // GET: /PrintEvents/
+        public ActionResult Index()
+        {
+            var printevents = db.PrintEvents.Include(p => p.Print).Include(p => p.Printer);
+            return View(printevents.ToList());
+        }
+
+        // GET: /PrintEvents/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PrintEvent printevent = db.PrintEvents.Find(id);
+            if (printevent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(printevent);
+        }
+
+        // GET: /PrintEvents/Create
+        public ActionResult Create()
+        {
+            ViewBag.PrintId = new SelectList(db.Prints, "PrintId", "FileName");
+            ViewBag.PrinterId = new SelectList(db.Printers, "PrinterId", "PrinterName");
+            return View();
+        }
+
+        // POST: /PrintEvents/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="PrintEventId,EventType,EventTimeStamp,MaterialUsed,PrinterId,UserName,PrintId")] PrintEvent printevent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PrintEvents.Add(printevent);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PrintId = new SelectList(db.Prints, "PrintId", "FileName", printevent.PrintId);
+            ViewBag.PrinterId = new SelectList(db.Printers, "PrinterId", "PrinterName", printevent.PrinterId);
+            return View(printevent);
+        }
+
+        // GET: /PrintEvents/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PrintEvent printevent = db.PrintEvents.Find(id);
+            if (printevent == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PrintId = new SelectList(db.Prints, "PrintId", "FileName", printevent.PrintId);
+            ViewBag.PrinterId = new SelectList(db.Printers, "PrinterId", "PrinterName", printevent.PrinterId);
+            return View(printevent);
+        }
+
+        // POST: /PrintEvents/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="PrintEventId,EventType,EventTimeStamp,MaterialUsed,PrinterId,UserName,PrintId")] PrintEvent printevent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(printevent).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PrintId = new SelectList(db.Prints, "PrintId", "FileName", printevent.PrintId);
+            ViewBag.PrinterId = new SelectList(db.Printers, "PrinterId", "PrinterName", printevent.PrinterId);
+            return View(printevent);
+        }
+
+        // GET: /PrintEvents/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PrintEvent printevent = db.PrintEvents.Find(id);
+            if (printevent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(printevent);
+        }
+
+        // POST: /PrintEvents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            PrintEvent printevent = db.PrintEvents.Find(id);
+            db.PrintEvents.Remove(printevent);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MakerFarm/Controllers/PrinterErrorTypesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MakerFarm.Models;
+
+namespace MakerFarm.Controllers
+{
+    public class PrinterErrorTypesController : Controller
+    {
+        private MakerfarmDBContext db = new MakerfarmDBContext();
+
+        // GET: /PrinterErrorTypes/
+        public ActionResult Index()
+        {
+            return View(db.PrintErrorTypes.ToList());
+        }
+
+        // GET: /PrinterErrorTypes/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PrintErrorType printerrortype = db.PrintErrorTypes.Find(id);
+            if (printerrortype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(printerrortype);
+        }
+
+        // GET: /PrinterErrorTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /PrinterErrorTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="PrintErrorTypeId,PrintErrorName,UserError")] PrintErrorType printerrortype)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PrintErrorTypes.Add(printerrortype);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(printerrortype);
+        }
+
+        // GET: /PrinterErrorTypes/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PrintErrorType printerrortype = db.PrintErrorTypes.Find(id);
+            if (printerrortype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(printerrortype);
+        }
+
+        // POST: /PrinterErrorTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="PrintErrorTypeId,PrintErrorName,UserError")] PrintErrorType printerrortype)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(printerrortype).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(printerrortype);
+        }
+
+        // GET: /PrinterErrorTypes/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PrintErrorType printerrortype = db.PrintErrorTypes.Find(id);
+            if (printerrortype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(printerrortype);
+        }
+
+        // POST: /PrinterErrorTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            PrintErrorType printerrortype = db.PrintErrorTypes.Find(id);
+            db.PrintErrorTypes.Remove(printerrortype);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
