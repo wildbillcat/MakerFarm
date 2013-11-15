387013d0c3f73b9e802fdce5cd46a5c8a25ec82f
Patrick McMorran <patrick.mcmorran@yale.edu>
2013-11-15 09:38:22 -0500

    working on tracking printer status's. Primming interface

--- MakerFarm/Controllers/PrintStatusLogsController.cs
+++ MakerFarm/Controllers/PrintStatusLogsController.cs
@@ -10,127 +10,118 @@ using MakerFarm.Models;
 
 namespace MakerFarm.Controllers
 {
-    public class PrintersController : Controller
+    public class PrintStatusLogsController : Controller
     {
         private MakerfarmDBContext db = new MakerfarmDBContext();
 
-        // GET: /Printers/
+        // GET: /PrintStatusLogs/
         public ActionResult Index()
         {
-            ViewBag.Title = "Printers";
-            Dictionary<int, PrinterType> PrinterTypes = db.PrinterTypes.ToDictionary(p => p.PrinterTypeId);
-            ViewBag.PrinterNames = PrinterTypes;
-            return View(db.Printers.ToList());
+            Dictionary<long, Printer> PrinterNames = db.Printers.ToDictionary(p => p.PrinterId);
+            ViewBag.PrinterNames = PrinterNames;
+            return View(db.PrinterStatusLogs.ToList());
         }
 
-        // GET: /Printers/Details/5
+        // GET: /PrintStatusLogs/Details/5
         public ActionResult Details(long? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Printer printer = db.Printers.Find(id);
-            ViewBag.Title = String.Concat("Details: ", printer.PrinterName);
-            if (printer == null)
+            PrinterStatusLog printerstatuslog = db.PrinterStatusLogs.Find(id);
+            if (printerstatuslog == null)
             {
                 return HttpNotFound();
             }
-            return View(printer);
+            return View(printerstatuslog);
         }
 
-        // GET: /Printers/Create
-        public ActionResult Create()
+        // GET: /PrintStatusLogs/Create
+        public ActionResult Create(long id = 0)
         {
-            ViewBag.Title = "Create Printer";
-            List<PrinterType> printerType = db.PrinterTypes.ToList();
-            ViewData["PrintersList"] = new SelectList(printerType, "PrinterTypeId", "TypeName");
-            if (printerType == null)
+            if (id == 0)
             {
-                //The printer you attempted to use does not exist in the database!
                 return RedirectToAction("Index", "PrinterTypes");
+            }
+            else
+            {
+                ViewData["PrinterID"] = id;
             }
             return View();
         }
 
-        // POST: /Printers/Create
+        // POST: /PrintStatusLogs/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include="PrinterId,PrinterName,PrinterTypeId")] Printer printer)
+        public ActionResult Create([Bind(Include="PrinterStatusLogId,LoggedPrinterStatus,Comment,PrinterId")] PrinterStatusLog printerstatuslog)
         {
+            printerstatuslog.LogEntryDate = DateTime.Now;
             if (ModelState.IsValid)
             {
-                db.Printers.Add(printer);
+                db.PrinterStatusLogs.Add(printerstatuslog);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(printer);
+            return View(printerstatuslog);
         }
 
-        // GET: /Printers/Edit/5
+        // GET: /PrintStatusLogs/Edit/5
         public ActionResult Edit(long? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ViewBag.Title = "Create Printer";
-            List<PrinterType> printerType = db.PrinterTypes.ToList();
-            ViewData["PrintersList"] = new SelectList(printerType, "PrinterTypeId", "TypeName");
-            if (printerType == null)
-            {
-                //The printer you attempted to use does not exist in the database!
-                return RedirectToAction("Index", "PrinterTypes");
-            }
-            Printer printer = db.Printers.Find(id);
-            if (printer == null)
+            PrinterStatusLog printerstatuslog = db.PrinterStatusLogs.Find(id);
+            if (printerstatuslog == null)
             {
                 return HttpNotFound();
             }
-            return View(printer);
+            return View(printerstatuslog);
         }
 
-        // POST: /Printers/Edit/5
+        // POST: /PrintStatusLogs/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="PrinterId,PrinterName,PrinterTypeId")] Printer printer)
+        public ActionResult Edit([Bind(Include="PrinterStatusLogId,LogEntryDate,LoggedPrinterStatus,Comment,PrinterId")] PrinterStatusLog printerstatuslog)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(printer).State = EntityState.Modified;
+                db.Entry(printerstatuslog).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(printer);
+            return View(printerstatuslog);
         }
 
-        // GET: /Printers/Delete/5
+        // GET: /PrintStatusLogs/Delete/5
         public ActionResult Delete(long? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Printer printer = db.Printers.Find(id);
-            if (printer == null)
+            PrinterStatusLog printerstatuslog = db.PrinterStatusLogs.Find(id);
+            if (printerstatuslog == null)
             {
                 return HttpNotFound();
             }
-            return View(printer);
+            return View(printerstatuslog);
         }
 
-        // POST: /Printers/Delete/5
+        // POST: /PrintStatusLogs/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(long id)
         {
-            Printer printer = db.Printers.Find(id);
-            db.Printers.Remove(printer);
+            PrinterStatusLog printerstatuslog = db.PrinterStatusLogs.Find(id);
+            db.PrinterStatusLogs.Remove(printerstatuslog);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
